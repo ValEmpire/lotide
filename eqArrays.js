026771ce6ec23468a19a5f19456a09a5cf6d8322
Arcival Palma <palma_arcival@yahoo.com>
2021-07-23 03:03:55 +0000

    perfect match function

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = (arr1, arr2) => {
+
+  let isPerfectMatch = true;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      isPerfectMatch = false;
+      break;
+    } else {
+      continue;
+    }
+  }
+
+  return isPerfectMatch;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
