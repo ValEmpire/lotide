bf0a7f07792fd8e978d8a07c2c29d9e374fc5cfd
Arcival Palma <palma_arcival@yahoo.com>
2021-07-28 14:41:51 +0000

    count onyl

---
+++ countOnly.js
@@ -0,0 +1,67 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// count all the same values in array
+const countAllValuesInArray = (allItems) => {
+  // init object
+  let allValueCounts = {};
+
+  // loop all the index in array and save the value as properties with value of counts
+  for (let i = 0; i < allItems.length; i++) {
+
+    if (allValueCounts[allItems[i]]) {
+      allValueCounts[allItems[i]] += 1;
+    } else {
+      allValueCounts[allItems[i]] = 1;
+    }
+
+  }
+
+  // return all valueCounts
+  return allValueCounts;
+}
+
+const countOnly = (allItems, itemsToCount) => {
+
+  const allValueCounts = countAllValuesInArray(allItems);
+
+  // init object
+  let totalItems = {};
+
+  // loop all prop of itemsToCount
+  for (const key in itemsToCount) {
+
+    // get only truthy properties
+    if (itemsToCount[key]) {
+
+      // save it as properties with value from returned countAllValuesInArray
+      totalItems[key] = allValueCounts[key];
+    }
+  }
+  return totalItems;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
