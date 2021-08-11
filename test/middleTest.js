3a1d030f630ad8da0406122b242dd11b7d9fef13
Arcival Palma <palma_arcival@yahoo.com>
2021-08-11 21:08:39 +0000

    mocha chai middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,19 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
+describe("#middle", () => {
+  it("returns empty array for arrays with less than or equal to 1", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-// TESTS
-assertArraysEqual([3], middle([1, 2, 3, 4, 5]));
+  it("returns an array with single middle element for arrays with odd number of elements ", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
 
-assertArraysEqual([true], [eqArrays([3, 4], middle([1, 2, 3, 4, 5, 6]))]);
+  it("returns an array containing the two elements in the middle for arrays with an even number of elements", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  })
 
-assertArraysEqual([], middle([2]));
-
-assertArraysEqual([], middle([]));
+});
